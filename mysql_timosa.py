f09d9853b3adeb8b07429a1fe0f8f1b4a00399bf
JovTim <202280218@psu.palawan.edu.ph>
2024-11-17 22:39:19 +0800

    fetch book from db

---
+++ mysql_timosa.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+import pymysql
+
+app = Flask(__name__)
+
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "root", # placeholder, not my real password
+    "database": "books_timosa",
+}
+
+def get_db_connection():
+    return pymysql.connect(**DB_CONFIG)
+
+def fetch_all_books():
+    connection = get_db_connection()
+    try:
+        with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+            cursor.execute("SELECT * FROM books")
+            return cursor.fetchall()
+    finally:
+        connection.close()
