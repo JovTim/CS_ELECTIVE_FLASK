f07e352a7334f5f81c67186e12c5d676d7b3a19c
JovTim <202280218@psu.palawan.edu.ph>
2024-11-17 22:47:12 +0800

    insert and delete from database

--- mysql_timosa.py
+++ mysql_timosa.py
@@ -31,3 +31,26 @@ def fetch_book_by_id(book_id):
             return cursor.fetchone()
     finally:
         connection.close()
+
+def update_book_in_db(book_id, data):
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "UPDATE books SET title = %s, author = %s, year = %s WHERE id = %s",
+                (data["title"], data["author"], data["year"], book_id),
+            )
+            connection.commit()
+            return cursor.rowcount > 0
+    finally:
+        connection.close()
+
+def delete_book_from_db(book_id):
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("DELETE FROM books WHERE id = %s", (book_id,))
+            connection.commit()
+            return cursor.rowcount > 0
+    finally:
+        connection.close()
